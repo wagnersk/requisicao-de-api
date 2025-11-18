de2fc5e3ed360ff88e3a534353d08517aefcd25f
Wagner <65842719+wagnersk@users.noreply.github.com>
2025-11-18 21:26:09 +0000

    FEATURE: implementando tanstack

---
+++ src/components/users-list.tsx
@@ -0,0 +1,16 @@
+import useUsers from "../hooks/use-users"
+
+export default function UsersList() {
+    const { users, isLoadingUsers } = useUsers()
+    if (isLoadingUsers) {
+        return <div>Carregando todos os usuários...</div>
+    }
+
+
+    return (
+        <ul>
+            {users.map(user => (
+                <li key={user.id}>Nome: {user.name}/ Username: {user.id} </li>
+            ))}
+        </ul>)
+}
